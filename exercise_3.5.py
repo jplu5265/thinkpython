84523402d87ddb13819d2d23eb7335909a53e11b
jplu5265 <42589352+jplu5265@users.noreply.github.com>
2018-09-21 16:16:44 -0400

    minor fix for function
    function do_four paramaters list change to be different from that of do_twice

--- exercise_3.5.py
+++ exercise_3.5.py
@@ -3,13 +3,15 @@
 def print_symbol(sym, val, terminator):
     print(sym * val + terminator,)
 
+#do_twice and do_four paramater lists must be distinct, error otherwise
+
 def do_twice(f, arg1, arg2, arg3):
     f(arg1, arg2, arg3)
     f(arg1, arg2, arg3)
 
 def do_four(g, par1, par2, par3):
-    do_twice(print_symbol, ' |      ', 2, '|')
-    do_twice(print_symbol, ' |      ', 2, '|')
+    do_twice(g, par1, par2, par3)
+    do_twice(g, par1, par2, par3)
 
 print_symbol('+----', 2, '+')
 do_four(print_symbol, ' |      ', 2, '|')
