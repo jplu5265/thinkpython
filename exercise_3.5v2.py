208456bc2a3f3c86f04f88b011a93b11607fba83
jplu5265 <42589352+jplu5265@users.noreply.github.com>
2018-09-21 17:20:42 -0400

    bug fixed
    use -= insteard of =-

--- exercise_3.5v2.py
+++ exercise_3.5v2.py
@@ -4,16 +4,16 @@ def print_symbol(sym, val, terminator):
     print(sym * val + terminator,)
 
 def print_break(val):
+    #print('val = %s' %val) 
     while val > 0:
         print_symbol('+----', 2, '+')
-        val =- 1
+        val -= 1
+        #print('val = %s' %val)
 
 def print_middle(num):
     while num > 0:
         print_symbol(' |      ', 2, '|')
-        num =- 1
-
-#do_twice and do_four paramater lists must be distinct, error otherwise
+        num -= 1
 
 def do_twice(f, arg1, arg2, arg3):
     f(arg1, arg2, arg3)
@@ -23,18 +23,8 @@ def do_four(g, par1, par2, par3):
     do_twice(g, par1, par2, par3)
     do_twice(g, par1, par2, par3)
 
-print_break(2)
+print_break(1)
 print_middle(4)
-'''
-def do_one_four(h, val1, val2, val3):
-    print_symbol(sym, val, terminator)
-    do_four(g, par1, par2, par3)
-
-do_one_four(print_symbol, 
-
-print_symbol('+----', 2, '+')
-do_four(print_symbol, ' |      ', 2, '|')
-print_symbol('+----', 2, '+')
-do_four(print_symbol, ' |      ', 2, '|')
-print_symbol('+----', 2, '+')
-'''
+print_break(1)
+print_middle(4)
+print_break(1)
